f6355e82b79345fbf50925deba9771a9033a70a2
farabi1 <rashidfarabi@gmail.com>
2022-04-04 17:14:48 +0600

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-     <h1 className='text-2xl'>Ninth Assignment Car Review</h1>
+      <h1 className='text-2xl'>Ninth Assignment Car Review</h1>
+
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+
+      </Routes>
+
     </div>
   );
 }
