336a603c1509fc83e30d2fb053b12346dbfaad3e
farabi1 <rashidfarabi@gmail.com>
2022-04-04 16:17:17 +0600

    Router Setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1 className='text-3xl'>Ninth Assignment Car Review</h1>
+     <h1 className='text-2xl'>Ninth Assignment Car Review</h1>
     </div>
   );
 }
